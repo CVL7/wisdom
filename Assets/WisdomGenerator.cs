f9338368516c1cca393d3bfddd2faa00899db93b
occult_culture <cyrilmolev@gmail.com>
2019-06-10 17:48:40 +0200

    Ho ho ho

--- Assets/WisdomGenerator.cs
+++ Assets/WisdomGenerator.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Text.RegularExpressions;
 using TMPro;
 using UnityEngine;
@@ -11,175 +12,141 @@ public class WisdomGenerator : MonoBehaviour
     public TextMeshProUGUI output;
     public TextAsset sourceText;
 
-    Dictionary<string, List<string>> dict = new Dictionary<string, List<string>>();
+    string[] corpus;
+    Dictionary<string, List<string>> model = new Dictionary<string, List<string>>();
     List<string> startingWords = new List<string>();
     int averageLineSize;
+
+    void Awake()
+    {
+        corpus = sourceText.text.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None);
+        corpus = Array.ConvertAll(corpus, x => x.ToLower());
+
+        var order = 3;
+        Learn(order);
+
+        GenerateAndShow();
+    }
+
+    public void GenerateAndShow()
+    {
+        var wisdomString = "";
+        while (string.IsNullOrEmpty(wisdomString))
+        {
+            wisdomString = ProcessGenerated(InternalGenerate());
+        }
+
+        output.text = wisdomString;
+    }
+
+    void Learn(int order)
+    {
+        var sb = new StringBuilder();
+        foreach (var line in corpus)
+        {
+            var tokens = Regex
+                .Split(line, @"(?=[.,!;?: ])|(?<=[.,!;?: ])")
+                .Where(x => !string.IsNullOrWhiteSpace(x))
+                .Select(x => x.Trim().ToLower())
+                .ToArray();
+
+            averageLineSize += tokens.Length;
+
+            if (order >= tokens.Length) continue;
+
+            var startKey = ConcatKey(0, order, tokens, sb);
+            startingWords.Add(startKey);
+
+            for (var i = 0; i < tokens.Length - order; i++)
+            {
+                var key = ConcatKey(i, order, tokens, sb);
+                var value = tokens[i + order];
+
+                List<string> tryValue;
+                model.TryGetValue(key, out tryValue);
+                if (tryValue != null)
+                {
+                    tryValue.Add(value);
+                }
+                else
+                {
+                    model.Add(key, new List<string>() { value });
+                }
+            }
+        }
+
+        averageLineSize /= corpus.Length;
+    }
+
+    List<string> InternalGenerate()
+    {
+        var wisdom = new List<string>();
+        var keyWords = GetRandom(startingWords);
+        wisdom.AddRange(keyWords.Split(' '));
+        while (true)
+        {
+            var valueWord = GetRandom(model[keyWords]);
+
+            // If we got a token that is already in sentence -> try decrease its chance and roll again
+            if (wisdom.Contains(valueWord))
+            {
+                var arrCopy = new List<string>(model[keyWords]);
+                var occurences = arrCopy.Where(x => x == valueWord).Count();
+                var removeCount = occurences > 1 ? occurences / 2 : 0;
+                for (var i = 0; i < removeCount; i++) arrCopy.Remove(valueWord);
+                valueWord = GetRandom(arrCopy);
+            }
+
+            wisdom.Add(valueWord);
+
+            if (valueWord == "." || valueWord == "!") break;
+
+            if (keyWords.IndexOf(' ') != -1) keyWords = keyWords.Substring(keyWords.IndexOf(' ') + 1) + " " + valueWord;
+            else keyWords = valueWord;
+        }
+
+        return wisdom;
+    }
+
+    string ProcessGenerated(List<string> wisdom)
+    {
+        // Check if too short or too long
+        if (wisdom.Count < averageLineSize - 1 || wisdom.Count > averageLineSize + 6)
+        {
+            InternalGenerate();
+            return "";
+        }
+
+        // Compose a string
+        var wisdomString = string.Join(" ", wisdom).Trim();
+        wisdomString = Regex.Replace(wisdomString, @"\s+(?=[.,?!:;])", "");
+
+        // Check if already exists
+        if (Array.IndexOf(corpus, wisdomString) >= 0)
+        {
+            InternalGenerate();
+            return "";
+        }
+
+        // Format and output
+        wisdomString = wisdomString.First().ToString().ToUpper() + wisdomString.Substring(1);
+        return wisdomString;
+    }
 
     string GetRandom(List<string> arr)
     {
         return arr[UnityEngine.Random.Range(0, arr.Count)];
     }
 
-    public void Generate()
+    string ConcatKey(int pos, int size, string[] arr, StringBuilder sb)
     {
-        var wisdom = new List<string>();
-        var word = GetRandom(startingWords);
-        wisdom.Add(word);
-
-        while (true)
+        sb.Clear();
+        for (int i = pos; i < pos + size - 1; i++)
         {
-            var previousWord = word;
-            word = GetRandom(dict[word]);
-
-            if (wisdom.Contains(word))
-            {
-                var arrCopy = new List<string>(dict[previousWord]);
-                var occurences = arrCopy.Where(x => x == word).Count();
-                var removeCount = occurences > 1 ? occurences / 2 : 0;
-                for (var i = 0; i < removeCount; i++)
-                {
-                    Debug.Log("Removing " + word);
-                    arrCopy.Remove(word);
-                }
-                Debug.Log("Array had " + occurences + " occurences of word " + word + " and now have only " + arrCopy.Where(x => x == word).Count());
-
-                word = GetRandom(arrCopy);
-            }
-
-            wisdom.Add(word);
-
-            if (word == "." || word == "!")
-            {
-                break;
-            }
-
-            word = previousWord.Split(' ')[1] + " " + word;
+            sb.Append(arr[i]);
+            sb.Append(" ");
         }
-
-        if ((wisdom.Count < (averageLineSize - 1)) || (wisdom.Count > (averageLineSize + 5)))
-        {
-            Debug.Log("Too long: " + wisdom.Count + " REGENERATING");
-            // Generate();
-            // return;
-        }
-
-        var wisdomString = "";
-        foreach (var part in wisdom)
-        {
-            var love = part.Length == 1 && Char.IsPunctuation(part[0]);
-            if (!love || part == "—") wisdomString += " ";
-            wisdomString += part;
-        }
-        wisdomString = wisdomString.Trim();
-        wisdomString = wisdomString.First().ToString().ToUpper() + wisdomString.Substring(1);
-
-        output.text = wisdomString;
+        sb.Append(arr[pos + size - 1]);
+        return sb.ToString();
     }
-
-    void Learn2()
-    {
-        var lines = sourceText.text.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None);
-
-        foreach (var line in lines)
-        {
-            var textArray = Regex.Split(line, @"(?=[.,!;?: ])|(?<=[.,!;?: ])");
-            textArray = textArray.Where(x => !string.IsNullOrWhiteSpace(x)).ToArray();
-            averageLineSize += textArray.Length;
-
-            if (char.IsUpper(textArray[0].Trim()[0])) startingWords.Add(textArray[0].Trim().ToLower() + " " + textArray[1].Trim().ToLower());
-
-            for (var i = 0; i < textArray.Length - 2; i++)
-            {
-                var first = textArray[i].Trim().ToLower();
-                var second = textArray[i + 1].Trim().ToLower();
-                var key = first + " " + second;
-                var third = textArray[i + 2].Trim().ToLower();
-
-                List<string> value;
-                dict.TryGetValue(key, out value);
-                if (value != null)
-                {
-                    //Debug.Log(third);
-                    value.Add(third);
-                }
-                else
-                {
-                    dict.Add(key, new List<string>() { third });
-                    //Debug.Log(key);
-                }
-            }
-        }
-
-
-        foreach (KeyValuePair<string, List<string>> kvp in dict)
-        {
-            Debug.Log(kvp.Key + " " + kvp.Value.Count);
-        }
-
-        averageLineSize /= lines.Length;
-        Debug.Log("Average size: " + averageLineSize);
-    }
-
-    void Learn()
-    {
-        var lines = sourceText.text.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None);
-
-        foreach (var line in lines)
-        {
-            var textArray = Regex.Split(line, @"(?=[.,!;?: ])|(?<=[.,!;?: ])");
-            textArray = textArray.Where(x => !string.IsNullOrWhiteSpace(x)).ToArray();
-            averageLineSize += textArray.Length;
-
-            if (char.IsUpper(textArray[0].Trim()[0])) startingWords.Add(textArray[0].Trim().ToLower());
-
-            for (var i = 0; i < textArray.Length - 1; i++)
-            {
-                var element = textArray[i].Trim().ToLower();
-                var nextElement = textArray[i + 1].Trim().ToLower();
-
-                List<string> value;
-                dict.TryGetValue(element, out value);
-                if (value != null)
-                {
-                    value.Add(nextElement);
-                }
-                else
-                {
-                    dict.Add(element, new List<string>() { nextElement });
-                }
-            }
-        }
-
-        averageLineSize /= lines.Length;
-        Debug.Log("Average size: " + averageLineSize);
-    }
-
-    void Awake()
-    {
-        Learn2();
-        Generate();
-        /* 
-        var textArray = Regex.Split(sourceText.text, @"(?=[.,!;?: ])|(?<=[.,!;?: ])");
-        textArray = textArray.Where(x => !string.IsNullOrWhiteSpace(x)).ToArray();
-
-        for (var i = 0; i < textArray.Length - 1; i++)
-        {
-            var element = textArray[i].Trim().ToLower();
-            var nextElement = textArray[i + 1].Trim().ToLower();
-
-            if (char.IsUpper(textArray[i].Trim()[0])) startingWords.Add(element);
-
-            List<string> value;
-            dict.TryGetValue(element, out value);
-            if (value != null)
-            {
-                value.Add(nextElement);
-            }
-            else
-            {
-                dict.Add(element, new List<string>() { nextElement });
-            }
-        }*/
-    }
-
 }
